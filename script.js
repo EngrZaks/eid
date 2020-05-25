17d66ca0a3c1643f3a860e0ebdd27103031ec575
EngrZaks <abzakariyya@gmail.com.com>
2020-05-25 20:31:34 +0100

    added support for browsers with no javascript

--- script.js
+++ script.js
@@ -1,41 +1,49 @@
-function debounce(func, wait = 20, immediate = true) {
-     var timeout;
-     return function () {
-          var context = this,
-               args = arguments;
-          var later = function () {
-               timeout = null;
-               if (!immediate) func.apply(context, args);
-          };
-          var callNow = immediate && !timeout;
-          clearTimeout(timeout);
-          timeout = setTimeout(later, wait);
-          if (callNow) func.apply(context, args);
-     };
-}
-
-const slidersImage = document.querySelectorAll("img");
-console.log(slidersImage);
-function checkslide(e) {
+window.addEventListener("load", function (e) {
+     const slidersImage = document.querySelectorAll("img");
+     // once the page is fully loaded, take away the display
      slidersImage.forEach((image) => {
-          // halfway throught the image
-          const slideInAt =
-               window.scrollY + window.innerHeight - image.height / 2;
-          // bottome of image
-          const imageButtom = image.offsetTop + image.height;
-          const isHalftShown = slideInAt > image.offsetTop;
-          const isNotScrolledPass = window.scrollY < imageButtom;
-
-          if (isHalftShown && isNotScrolledPass) {
-               if (image.classList.contains("left")) {
-                    image.classList.add("active2");
-               } else {
-                    image.classList.add("active");
-               }
-          } else {
-               image.classList.remove("active");
-          }
+          image.classList.add("nodisplay");
      });
-}
+     function debounce(func, wait = 20, immediate = true) {
+          var timeout;
+          return function () {
+               var context = this,
+                    args = arguments;
+               var later = function () {
+                    timeout = null;
+                    if (!immediate) func.apply(context, args);
+               };
+               var callNow = immediate && !timeout;
+               clearTimeout(timeout);
+               timeout = setTimeout(later, wait);
+               if (callNow) func.apply(context, args);
+          };
+     }
 
-window.addEventListener("scroll", debounce(checkslide));
+     function checkslide(e) {
+          slidersImage.forEach((image) => {
+               // halfway throught the image
+               const slideInAt =
+                    window.scrollY + window.innerHeight - image.height / 2;
+               // bottome of image
+               const imageButtom = image.offsetTop + image.height;
+               const isHalftShown = slideInAt > image.offsetTop;
+               const isNotScrolledPass = window.scrollY < imageButtom;
+
+               if (isHalftShown && isNotScrolledPass) {
+                    if (image.classList.contains("left")) {
+                         image.classList.remove("nodisplay");
+                         image.classList.add("active2");
+                    } else {
+                         image.classList.remove("nodisplay");
+                         image.classList.add("active");
+                    }
+               } else {
+                    image.classList.add("nodisplay");
+                    image.classList.remove("active");
+               }
+          });
+     }
+
+     window.addEventListener("scroll", debounce(checkslide));
+});
